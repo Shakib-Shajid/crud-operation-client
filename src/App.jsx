e29d44980d1823019fefd3819d5d2f67dd8ce014
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-06-28 04:04:37 +0600

    form designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useEffect } from "react"
 import { useState } from "react"
+import { IoIosSearch } from "react-icons/io";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { IoKeySharp } from "react-icons/io5";
+
+
+
 
 function App() {
 
@@ -15,6 +22,28 @@ function App() {
     <div className="text-center m-20 md:m-52 text-xl md:text-3xl">
       <h3 className="rounded-xl font-bold p-5 bg-indigo-400 text-black">*** User Manager ***</h3>
       <p className="my-3 bg-orange-600 text-white rounded-xl w-2/3 mx-auto p-3">Number of users: {users.length}</p>
+
+      <div className="m-10 w-2/3 mx-auto p-10 border-2 border-red-500 rounded-xl">
+      <h3 className="text-2xl text-center font-bold">Fill up the Form</h3>
+        <label className="input input-bordered border-2 flex items-center gap-2 my-5">
+          <input type="text" className="grow" placeholder="Search" />
+          <IoIosSearch />
+        </label>
+        <label className="input input-bordered border-2 flex items-center gap-2 my-5">
+          <MdOutlineEmail />
+          <input type="text" className="grow" placeholder="Email" />
+        </label>
+        <label className="input input-bordered border-2 flex items-center gap-2 my-5">
+          <FaUser />
+          <input type="text" className="grow" placeholder="Username" />
+        </label>
+        <label className="input input-bordered border-2 flex items-center gap-2 my-5">
+          <IoKeySharp />
+          <input type="password" className="" placeholder="Password" value="" />
+        </label>
+        <button className="btn btn-success w-full ">Success</button>
+
+      </div>
       <div className="grid grid-cols-3 gap-5 ">
         {
           users.map(user =>
