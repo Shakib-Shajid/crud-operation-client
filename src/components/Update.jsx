47bcfec56b6803b105bffbfca4054d36b61e77a5
shakib-shajid <shakibalshajid1682@gmail.com>
2024-11-17 01:37:31 +0600

    used axios in update function

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -2,65 +2,76 @@ import { useLoaderData, useNavigate } from "react-router-dom";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
+import axios from "axios";
 
 const Update = () => {
 
-    const loadedUsers = useLoaderData();
-    const navigate = useNavigate();
-    // const location = useLocation();
-    // console.log(location);
+  const loadedUsers = useLoaderData();
+  const navigate = useNavigate();
+  // const location = useLocation();
+  // console.log(location);
 
-    // const form = 
-    
-    const handleUpdate = (event) => {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        const email = form.email.value;
-        const number = form.number.value;
-        const updatedUser = { name, email, number };
-    
-        fetch(`http://localhost:5000/users/${loadedUsers._id}`, {
-          method: "PUT",
-          headers: { 
-            'content-type': "application/json"
-           },
-          body: JSON.stringify(updatedUser)
-        })
-          .then(res => res.json())
-          .then(data => {
-            console.log(data);
-            if(data.modifiedCount > 0){
-              alert("User updated Successfully")
-              form.reset();
-              navigate("/users");
-            }
-          })
-      }
-    
-    
-    return (
-        <div>
-            <div className="m-5 w-2/3 mx-auto p-10 border-2 border-red-500 rounded-xl">
+  // const form = 
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const number = form.number.value;
+    const updatedUser = { name, email, number };
+
+    // fetch(`http://localhost:5000/users/${loadedUsers._id}`, {
+    //   method: "PUT",
+    //   headers: { 
+    //     'content-type': "application/json"
+    //    },
+    //   body: JSON.stringify(updatedUser)
+    // })
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     console.log(data);
+    //     if(data.modifiedCount > 0){
+    //       alert("User updated Successfully")
+    //       form.reset();
+    //       navigate("/users");
+    //     }
+    //   })
+
+    axios.put(`http://localhost:5000/users/${loadedUsers._id}`, updatedUser)
+      .then(data => {
+        if (data.data.modifiedCount > 0) {
+          alert("User updated Successfully")
+          form.reset();
+          navigate("/users");
+        }
+      })
+      .catch(error => console.log(error))
+  }
+
+
+  return (
+    <div>
+      <div className="m-5 w-2/3 mx-auto p-10 border-2 border-red-500 rounded-xl">
         <h3 className="text-2xl text-center font-bold">Update info of {loadedUsers.name}</h3>
         <form onSubmit={handleUpdate}>
           <label className="input input-bordered border-2 flex items-center gap-2 my-5">
             <MdOutlineEmail />
-            <input type="email" name="email" className="grow" placeholder="Email" defaultValue={loadedUsers?.email}/>
+            <input type="email" name="email" className="grow" placeholder="Email" defaultValue={loadedUsers?.email} />
           </label>
           <label className="input input-bordered border-2 flex items-center gap-2 my-5">
             <FaUser />
-            <input type="text" name="name" className="grow" placeholder="Username" defaultValue={loadedUsers?.name}/>
+            <input type="text" name="name" className="grow" placeholder="Username" defaultValue={loadedUsers?.name} />
           </label>
           <label className="input input-bordered border-2 flex items-center gap-2 my-5">
             <FaPhoneAlt />
-            <input type="number" name="number" className="grow" placeholder="Phone Number" defaultValue={loadedUsers?.number}/>
+            <input type="number" name="number" className="grow" placeholder="Phone Number" defaultValue={loadedUsers?.number} />
           </label>
           <input type="submit" className="btn btn-success w-full" value="submit" />
         </form>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Update;
